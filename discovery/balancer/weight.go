33c10028dd9f59b753bbf16a1ae225524399e86b
jinhui <jinhui01@yungengxin.com>
2024-03-15 16:08:24 +0800

    Update LICENSE desc.

--- discovery/balancer/weight.go
+++ discovery/balancer/weight.go
@@ -1,5 +1,5 @@
 /* Copyright (c) 2024 jhuix. All rights reserved.
- * Use of this source code is governed by a MIT license
+ * Use of this source code is governed by a license
  * that can be found in the LICENSE file.
  */
 
--- discovery/common/attributes/attributes.go
+++ discovery/common/attributes/attributes.go
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2024 jhuix. All rights reserved.
- * Use of this source code is governed by a MIT license
+ * Use of this source code is governed by a license
  * that can be found in the LICENSE file.
  */
 
