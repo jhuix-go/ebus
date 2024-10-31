54e88031ec45eb0a36d6a55aa43b08e4ef86f354
jinhui <jinhui01@yungengxin.com>
2024-10-31 17:38:45 +0800

    Add version control file

--- main.go
+++ main.go
@@ -7,10 +7,23 @@
 package main
 
 import (
-	`github.com/jhuix-go/ebus/ebus`
-	`github.com/jhuix-go/ebus/pkg/app`
-	`github.com/jhuix-go/ebus/server`
+	"github.com/jhuix-go/ebus/ebus"
+	"github.com/jhuix-go/ebus/pkg/app"
+	"github.com/jhuix-go/ebus/server"
 )
+
+func init() {
+	app.Version = Version
+	if len(GoVersion) > 0 {
+		app.GoVersion = GoVersion
+	}
+	if len(BuildTime) > 0 {
+		app.BuildTime = BuildTime
+	}
+	if len(CommitHash) > 0 {
+		app.CommitHash = CommitHash
+	}
+}
 
 func main() {
 	app.Start(server.SelfName, app.ConfigTypeToml, ebus.NewService())
---
+++ version.go
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 jhuix. All rights reserved.
+ * Use of this source code is governed by a license
+ * that can be found in the LICENSE file.
+ */
+
+package main
+
+var (
+	Version    = "v0.2.0" // ebus service version
+	GoVersion  = ""
+	BuildTime  = ""
+	CommitHash = ""
+)
